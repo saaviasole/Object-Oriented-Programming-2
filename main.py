9e1e21197200df29b64020f4948f7c04a1fe0fb5
saaviasole <swapnaliasole@yahoo.com>
2026-01-16 17:34:56 +0530

    library

--- main.py
+++ main.py
@@ -16,3 +16,41 @@ class student:
 
 s1 = student("Saavi")
 del s1
+
+
+class Library:
+    def __init__(self):
+        self.books = ["Python", "HTML", "C++", "CSS"]
+
+    def display_books(self):
+        if self.books:
+            print("\nAvailable Books:")
+            for book in self.books:
+                print("-", book)
+        else:
+            print("\nNo books available.")
+
+    def lend_book(self, book_name):
+        if book_name in self.books:
+            self.books.remove(book_name)
+            print(f"\nYou have claimed the book '{book_name}'.")
+        else:
+            print("\nSorry, Book not avalable")
+
+    def return_book(elf,book_name):
+        self.books.append(book_name)
+        print(f"\n'{book_name}' has been returned")
+
+    def add_book(self, book_name):
+        self.book.append(book_name)
+        print(f"\n'{book_name}' has been added to the library.")
+
+library = Library()
+
+while True:
+    print("\n--- Library Manaagement System")
+    print("1. Dispplay Books")
+    print("2. Lend Book")
+    print("3. Retuen Book")
+    print("4. Add Book")
+    print("5. Exit")
